24c27d25fde0d101b82ee5e4dd9822293ca1caaa
mohammad95-crypto <mohammadhadighorghi@gmail.com>
2026-08-24 10:32:41 +0330

    Fix main.py keep alive

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("GHOLMONG Alert Bot")
+from flask import Flask
+import threading
+import time
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "GHOLMONG X Alert Bot is running!"
+
+def keep_alive():
+    app.run(host="0.0.0.0", port=10000)
+
+if __name__ == "__main__":
+    threading.Thread(target=keep_alive).start()
+
+    while True:
+        print("GHOLMONG Alert Bot Running...")
+        time.sleep(60)
